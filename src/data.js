7fc0f0a42cf09cdc2fae6e359c11a078241ab1ae
Rawisara Saman <rawiisara@gmail.com>
2022-04-08 23:33:27 +0200

    added data object

---
+++ src/data.js
@@ -0,0 +1,116 @@
+export const featuredProjects = [
+    {
+        id: 1,
+        title: 'Github Tracker',
+        name: 'Github tracker with Github REST API',
+        img: '',
+        description: 'This Github tracker is built with Vanilla JavaScript. The purpose is to track all the projects built during Technigo Frontend Development Bootcamp and to dig deeper into using APIs.', 
+        netlify: '',
+        github: '',
+        tags: ['HTML5', 'CSS3', 'JavaScript ES6', 'API']
+    },
+    {
+        id: 2,
+        title: 'Guess Who?',
+        name: 'Guess-who-game: French philosophers edition',
+        img: '',
+        description: 'A tweak of the classic game "Guess Who?" built with JavaScript ES6 which adds my personal touch in the project.', 
+        netlify: '',
+        github: '',
+        tags: ['HTML5', 'CSS3', 'JavaScript ES6']
+    },
+    {
+        id: 3,
+        title: 'Popular Movies',
+        name: 'Movies Site built with React.',
+        img: '',
+        description: 'Working with API requests was fun! Users can see details of each movie thanks to React Router.', 
+        netlify: '',
+        github: '',
+        tags: ['React', 'Styled Components', 'API', 'Pair-programming']
+    },
+    {
+        id: 4,
+        title: 'Happy recycling!',
+        name: 'A quiz built with React and Redux that inspires Stockholmers to recycle correctly.',
+        img: '',
+        description: '', 
+        netlify: '',
+        github: '',
+        tags: ['HTML5', 'CSS3', 'React', 'Redux', 'Mob-programming']
+    },
+    {
+        id: 5,
+        title: 'Github Tracker',
+        name: '',
+        img: '',
+        description: '', 
+        netlify: '',
+        github: '',
+        tags: ['HTML5', 'CSS3', 'JavaScript ES6']
+    },
+    {
+        id: 6,
+        title: 'Github Tracker',
+        name: '',
+        img: '',
+        description: '', 
+        netlify: '',
+        github: '',
+        tags: ['HTML5', 'CSS3', 'JavaScript ES6']
+    },
+]
+
+export const otherProjects = [
+    {
+        id: 1, 
+        project: '',
+        description: '',
+        tags: ['']
+    },
+    {
+        id: 2, 
+        project: '',
+        description: '',
+        tags: ['']
+    },
+    {
+        id: 3, 
+        project: '',
+        description: '',
+        tags: ['']
+    },
+    {
+        id: 4, 
+        project: '',
+        description: '',
+        tags: ['']
+    },
+    {
+        id: 5, 
+        project: '',
+        description: '',
+        tags: ['']
+    },
+    {
+        id: 6, 
+        project: '',
+        description: '',
+        tags: ['']
+    }
+]
+
+export const articles = [
+    {
+        title: '',
+        preview: '',
+        img: '',
+        date: ''
+    },
+    {
+        title: '',
+        preview: '',
+        img: '',
+        date: ''
+    },
+]
